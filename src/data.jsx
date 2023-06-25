13033f906eb92ad1a912278b0bc38c81c48becef
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-25 05:29:29 +0100

    update

---
+++ src/data.jsx
@@ -0,0 +1,12 @@
+const peopleData = [
+  {
+    image: "./assets/images/hip1.jpg",
+    name: "Bertie Yates",
+    age: 29,
+  },
+  { image: "./assets/images/hi2.jpg", name: "Hester Hogan", age: 32 },
+  { image: "./assets/images/hip3.jpg", name: "Larry Little", age: 36 },
+  { image: "./assets/images/logo.jpg", name: "Sean Walsh", age: 34 },
+  { image: "./assets/images/user1.jpg", name: "Lola Gardner", age: 29 },
+];
+export default peopleData;
